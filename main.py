5eb9188aa7ddddf2635c8dfbf034a4136b155e63
Ney08 <neybi028@gmail.com>
2026-06-04 03:02:19 -0400

    testing

--- main.py
+++ main.py
@@ -32,16 +32,15 @@ if not os.path.exists("facturas"):
 # ✅ CORS
 
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # ✅ DB
